02631e51ca9b79e0d81e945cb0d00ddb5e75ce89
Antony Derham <antonyderham@gmail.com>
2015-02-27 15:36:52 +0000

    Added "Referred By" field to registration screen

--- Referrals.integration.php
+++ Referrals.integration.php
@@ -14,7 +14,7 @@ if (!defined('ELK'))
 
 function ia_referrals(&$actionArray, &$adminActions)
 {
-	$actionsArray['referral'] = array('Referrals.controller.php', 'Referrals_Controller', 'action_index');
+	$actionArray['referral'] = array('Referrals.controller.php', 'Referrals_Controller', 'action_index');
 }
 
 function ira_referrals($regOptions, $memberID)
@@ -36,7 +36,7 @@ function ira_referrals($regOptions, $memberID)
 		$db->query('', '
 			UPDATE {db_prefix}members
 			SET referred_by = {int:referred_by}, referred_date = {int:date}
-			WHERE id_member = {int:member_id}'
+			WHERE id_member = {int:member_id}',
 			array(
 				'referred_by' => $referredBy,
 				'date' => time(),
@@ -45,4 +45,34 @@ function ira_referrals($regOptions, $memberID)
 		);
 
 	}
+}
+
+function ilcpf_referrals($memID, $area)
+{
+	global $context;
+
+	if ($area != 'register' || !isset($_SESSION['referred_by']) || empty($_SESSION['referred_by']))
+		return;
+
+	$db = database();
+
+	$request = $db->query('', '
+		SELECT real_name FROM {db_prefix}members
+		WHERE id_member = {int:member_id}',
+		array(
+			'member_id' => $_SESSION['referred_by'],
+		)
+	);
+	$member = $db->fetch_assoc($request);
+	$db->free_result($request);
+
+	$context['custom_fields'][] = array(
+			'name' => 'Referred By',
+			'type' => 'text',
+			'input_html' => '<input type="text" value="'.$member['real_name'].'" name="customfield[referred_by]" readonly />',
+			'colname' => 'referred_by',
+			'value' => $_SESSION['referred_by'],
+			'show_reg' => 2,
+	);
+	$context['custom_fields_required'] = true;
 }
